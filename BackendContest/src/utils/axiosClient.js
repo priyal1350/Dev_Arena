35404e8d243b05bfac6af0dfd6a9d7e970b8c220
priyal1350 <163632872+priyal1350@users.noreply.github.com>
2025-11-09 12:08:05 +0530

    Add files via upload

---
+++ BackendContest/src/utils/axiosClient.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const axiosClient =  axios.create({
+    baseURL: 'http://localhost:3000',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+        
+    }
+});
+
+
+module.exports = axiosClient;
+
